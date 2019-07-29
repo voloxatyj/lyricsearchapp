e9ed52c1e0dd64d6b4f963950ee1829156b5155b
Ostik <ostapmedunskij@gmail.com>
2019-07-29 19:31:00 +0300

    Updates

---
+++ precache-manifest.79326f0ee2901ba86a10be8b0704fb68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d582caaf8a55480b172022e2b5841aa",
+    "url": "/lyricsearchapp/index.html"
+  },
+  {
+    "revision": "ee9b5d161b71bb10a4ae",
+    "url": "/lyricsearchapp/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "001e0eca61cfd9b4870b",
+    "url": "/lyricsearchapp/static/js/2.1a3a55ec.chunk.js"
+  },
+  {
+    "revision": "ee9b5d161b71bb10a4ae",
+    "url": "/lyricsearchapp/static/js/main.b5038de9.chunk.js"
+  },
+  {
+    "revision": "eeed63484f7f2a31d8a1",
+    "url": "/lyricsearchapp/static/js/runtime~main.871df73f.js"
+  }
+]);
